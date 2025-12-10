af4646f752de61e66e14b7de8a05a65ab33d9901
Hemanth <69238080+hemanthgs@users.noreply.github.com>
2025-12-10 08:40:27 +0530

    Enhance run.bat with timestamp and error checks
    Updated date format in filename and added error handling for file copy operations.

--- run.bat
+++ run.bat
@@ -5,7 +5,7 @@ set count=0
 set CurrentDir=%CD%
 
 :: 1. Get date using PowerShell (Format: yyyy-MM-dd)
-for /f %%a in ('powershell -Command "Get-Date -format yyyyMMdd"') do set "Today=%%a"
+for /f %%a in ('powershell -Command "Get-Date -format yyyyMMdd_hhmmss"') do set "Today=%%a"
 
 :: 2. Add the prefix
 set MyFile=Pic
@@ -16,9 +16,16 @@ echo The prefix filename is: %PrefixedFile%
 C:
 cd C:\Users\heman\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets
 FOR %%i IN (*) DO (
-
- echo Copying %CurrentDir%\%PrefixedFile%_!count!.jpg
- copy %%i %CurrentDir%\%PrefixedFile%_!count!.jpg
+  echo "Copying %%i --> %PrefixedFile%_!count!.jpg"
+  copy %%i "%CurrentDir%\%PrefixedFile%_!count!.jpg" > nul
+ 
+  if %ERRORLEVEL% NEQ 0 (
+    echo FAILED: Error code: %ERRORLEVEL%
+    echo ----------------------------------
+  ) else ( 
+    echo SUCCESSFUL: Copied
+    echo ----------------------------------
+  )
  
  :: Increment the variable by 1
  set /a count=!count!+1
